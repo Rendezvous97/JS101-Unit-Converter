7941770077de4bd77a951e086c936e1fc6033444
Swagam Dasgupta <swagam.dasgupta@gmail.com>
2022-10-13 21:58:53 +0530

    Refactored previous code to be cleaner

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-let lengthOutput = document.getElementById("length-output")
-let volumeOutput = document.getElementById("volume-output")
-let massOutput = document.getElementById("mass-output")
+// let lengthOutput = document.getElementById("length-output")
+// let volumeOutput = document.getElementById("volume-output")
+// let massOutput = document.getElementById("mass-output")
 const convertBtn = document.getElementById("convert-btn")
 const inputValue = document.getElementById("number-to-convert")
 
 
-function unitConverter(unit){
-    let caption = ""
-    if (unit=="length"){
-        const meterToFeet = (inputValue.value*3.281).toFixed(3)
-        const feetToMeter = (inputValue.value/3.281).toFixed(3)
-        caption = `${inputValue.value} meters = ${meterToFeet} feet | 
-        ${inputValue.value} feet = ${feetToMeter} meters`
-    } else if (unit=="volume"){
-        const litresToGallons = (inputValue.value*0.264).toFixed(3)
-        const gallonstoLitres = (inputValue.value/0.264).toFixed(3)
-        caption = `${inputValue.value} litres = ${litresToGallons} gallons | 
-        ${inputValue.value} gallons = ${gallonstoLitres} litres`
-    } else {
-        const kilosToPounds = (inputValue.value*2.204).toFixed(3)
-        const poundsToKilos = (inputValue.value/2.204).toFixed(3)
-        caption = `${inputValue.value} kilos = ${kilosToPounds} pounds | 
-        ${inputValue.value} pounds = ${poundsToKilos} kilos`
-    }
+// --------------------------------------------------------------------------
+// Method 1
+// --------------------------------------------------------------------------
+// function unitConverter(unit){
+//     let caption = ""
+//     if (unit=="length"){
+//         const meterToFeet = (inputValue.value*3.281).toFixed(3)
+//         const feetToMeter = (inputValue.value/3.281).toFixed(3)
+//         caption = `${inputValue.value} meters = ${meterToFeet} feet | 
+//         ${inputValue.value} feet = ${feetToMeter} meters`
+//     } else if (unit=="volume"){
+//         const litresToGallons = (inputValue.value*0.264).toFixed(3)
+//         const gallonstoLitres = (inputValue.value/0.264).toFixed(3)
+//         caption = `${inputValue.value} litres = ${litresToGallons} gallons | 
+//         ${inputValue.value} gallons = ${gallonstoLitres} litres`
+//     } else {
+//         const kilosToPounds = (inputValue.value*2.204).toFixed(3)
+//         const poundsToKilos = (inputValue.value/2.204).toFixed(3)
+//         caption = `${inputValue.value} kilos = ${kilosToPounds} pounds | 
+//         ${inputValue.value} pounds = ${poundsToKilos} kilos`
+//     }
     
-    return caption
-}
+//     return caption
+// }
 
-convertBtn.addEventListener("click", function(){
-    lengthOutput.textContent = unitConverter("length")
-    volumeOutput.textContent = unitConverter("volume")
-    massOutput.textContent = unitConverter("mass")
+// convertBtn.addEventListener("click", function(){
+//     lengthOutput.textContent = unitConverter("length")
+//     volumeOutput.textContent = unitConverter("volume")
+//     massOutput.textContent = unitConverter("mass")
     
-    inputValue.value = ""
-})
+//     inputValue.value = ""
+// })
 
 
-
+// --------------------------------------------------------------------------
+// Method 2
+// --------------------------------------------------------------------------
 
 function convertUnit(unit1, unit2, multiplier, id){
     const conversionOne = (inputValue.value * multiplier).toFixed(3)
@@ -53,7 +58,9 @@ function displayUnit(caption, id){
 }
 
 convertBtn.addEventListener("click", function(){
-    convertUnit("")
+    convertUnit("meters", "feet", 3.281, "length-output")
+    convertUnit("liter", "gallon", 0.264, "volume-output")
+    convertUnit("kilogram", "pound", 2.204, "mass-output")
     
     inputValue.value = ""
 })
